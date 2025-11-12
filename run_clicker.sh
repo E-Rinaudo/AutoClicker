eec4fff4ef564c7e84c506fbcef899a6553e5de1
enricorinaudo <erinaudo.git@w.ernode.com>
2025-11-12 21:57:01 +0100

    Fix run_clicker file not found error.

--- run_clicker.sh
+++ run_clicker.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/venv/bin/activate"
 
 # Run with the venv python.
-"${VIRTUAL_ENV:-$SCRIPT_DIR/venv}/bin/python" "${SCRIPT_DIR}main.py" "$@"
+"${VIRTUAL_ENV:-$SCRIPT_DIR/venv}/bin/python" "${SCRIPT_DIR}/main.py" "$@"
